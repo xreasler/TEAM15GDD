7e8a7c0591366f3bf633a9aa84405ad89adc11c4
Bazzett <elias.strandberg@edu.futuregames.nu>
2022-11-18 16:36:49 +0100

    Made velocity private again

--- Assets/StarterAssets/ThirdPersonController/Scripts/ThirdPersonController.cs
+++ Assets/StarterAssets/ThirdPersonController/Scripts/ThirdPersonController.cs
@@ -84,7 +84,7 @@ namespace StarterAssets
         private float _animationBlend;
         private float _targetRotation = 0.0f;
         private float _rotationVelocity;
-        public float _verticalVelocity;
+        private float _verticalVelocity;
         private float _terminalVelocity = 53.0f;
 
         // timeout deltatime
