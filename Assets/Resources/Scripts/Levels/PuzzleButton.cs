1f06f0dea7eb944a725e63835f436fe7bba86a32
xreasler <j.o.n.t.e@hotmail.com>
2022-12-02 11:31:07 +0100

    eee

--- Assets/Resources/Scripts/Levels/PuzzleButton.cs
+++ Assets/Resources/Scripts/Levels/PuzzleButton.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,14 +9,35 @@ public class PuzzleButton : MonoBehaviour, IDamageable
     
     public Transform Handler;
     public bool HaveCounted = false;
-    
+    public Material[] mainMat;
+
+    Renderer rend;
+    public static int i;
+
+
+    public void Start()
+    {
+        rend = GetComponent<Renderer> ();
+    }
+
     public void Damage()
     {
         if (HaveCounted == false)
         {
             Debug.Log("BUTTON HIT CURRENT COUNT");
             HaveCounted = true;
+            
             Handler.GetComponent<PuzzleDoor>().AddCount(); 
+            if (i == 0)
+            {
+                rend.sharedMaterial = mainMat[0];
+                i++;
+            }
+            else if (i == 1)
+            {
+                rend.sharedMaterial = mainMat[1];
+                i--;
+            }  
         }
         
         
